681ec42a11dc3dab275b3b2cf3fcec167306c679
Victor <vbond02@qub.ac.uk>
2025-10-23 17:23:24 +0100

    Testing pump protocols - first try

--- Pump.py
+++ Pump.py
@@ -12,17 +12,17 @@ class AL1000:
         if self.ser.isOpen():
             self.ser.timeout = 1
             print("Device is connected")
-            log_action('device_log.txt', "Connection to Knauer pump successful.")
+            #log_action('device_log.txt', "Connection to Knauer pump successful.")
 
         else:
             print ('The Port is closed: ' + self.ser.portstr)
-            log_action('device_log.txt', "Connection to Knauer pump failed.")
+            #log_action('device_log.txt', "Connection to Knauer pump failed.")
         
     def command(self, code):
         """ Sends command to device in bytes and retrieves the response """
         #sending the command to device
         self.ser.write(f'{code}\r'.encode())
         #accepting the response
-        byteData = self.ser.readline().decode().strip()
+        #byteData = self.ser.readline().decode().strip()
         #give response
-        return byteData
+        #return byteData
